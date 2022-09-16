41bb9fc34ba1048f9f1525ec7857c92180784f26
Thiago Bezerra de Carvalho <tb161372@gmail.com>
2022-09-16 20:33:25 -0300

    foi compçicado

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿string nomezin, sobrenome;
+Console.WriteLine("escreva seu nome aí...");
+nomezin = Console.ReadLine()!;
+
+Console.WriteLine("agora, seu sobrenome...");
+sobrenome = Console.ReadLine()!;
+
+Console.WriteLine($"seu nome completo: {nomezin} {sobrenome} ^^ ");
+Console.WriteLine($"seu nome catálogo: {sobrenome.ToUpper()}, {nomezin} ^^");
